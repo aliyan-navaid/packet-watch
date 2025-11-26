1c7de357ed68e33b74afc0ad5e44c398df6903e5
aliyan-navaid <aliyan.navaid@gmail.com>
2025-11-26 18:54:57 +0500

    feat: add events

--- app/utils/__init__.py
+++ app/utils/__init__.py
@@ -1,3 +1,4 @@
 from app.utils.events import Event
 from app.utils.interfaces import Subject, Observer
 from app.utils.metrics import *
+from app.utils.models import PacketData, MetricsSnapshot, AlertInfo
--- app/utils/events.py
+++ app/utils/events.py
@@ -1,4 +1,19 @@
+from app.utils import PacketData, MetricsSnapshot, AlertInfo
+from typing import Union
+
 class Event:
-    def __init__(self, name: str, payload: dict = None):
+    def __init__(self, name: str, payload: Union[PacketData, MetricsSnapshot, AlertInfo]):
         self.name = name
-        self.payload = payload or {}
+        self.payload = payload
+
+class PacketCapturedEvent(Event):
+    def __init__(self, packet_data: PacketData):
+        super().__init__("packet_captured", packet_data)
+
+class MetricsUpdatedEvent(Event):
+    def __init__(self, metrics_snapshot: MetricsSnapshot):
+        super().__init__("metrics_updated", metrics_snapshot)
+
+class AlertGeneratedEvent(Event):
+    def __init__(self, alert_info: AlertInfo):
+        super().__init__("alert_generated", alert_info)
